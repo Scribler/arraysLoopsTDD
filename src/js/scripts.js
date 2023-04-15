42df2d07d8a8ad53e5d77ec271506e2a39790201
Steven McIntyre <steven.mc2225@gmail.com>
2023-04-15 11:14:36 -0700

    starting lesson

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,15 @@
 console.log("hello World");
+
+
+const backPack = [];
+backPack.push("rag");
+backPack.push("rope");
+backPack.push("pan");
+
+console.log(backPack);
+console.log(backPack.indexOf('rope'));
+
+console.log(Array.isArray(backPack)); // test if something is an array (true)
+console.log(backPack instanceof Array); // test if something is an array (true)
+console.log(backPack instanceof String); // test if something is an array (false)
+
