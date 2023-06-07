2172887340edcd5959b946215eaac9dfef0076df
Steven McIntyre <steven.mc2225@gmail.com>
2023-06-07 14:20:32 -0700

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,67 @@
-console.log("hello World");
+const arrOut = document.getElementById('arrayOutput');
 
-
+//
+// Arrays Area
+//
 const backPack = [];
 backPack.push("rag");
 backPack.push("rope");
 backPack.push("pan");
 
-console.log(backPack);
-console.log(backPack.indexOf('rope'));
+// DISPLAY ARRAY'S CURRENT STATE
+arrOut.innerHTML += `<h3>Contents of Backpack: <span class="posAnswer">${backPack.toString()}</span></h3>`;
+arrOut.innerHTML += `<br>`
 
-console.log(Array.isArray(backPack)); // test if something is an array (true)
-console.log(backPack instanceof Array); // test if something is an array (true)
-console.log(backPack instanceof String); // test if something is an array (false)
+
+// .toString()
+arrOut.innerHTML += "<h3>.toString() / .join( 'delineator' )</h3>";
+arrOut.innerHTML += `<p> - arrayName.toString(): <span class="posAnswer">${backPack.toString()}</span></p>`;
+
+// .join()
+arrOut.innerHTML += `<p> - arrayName.join(" ** "): <span class="posAnswer">${backPack.join(" ** ")}</span></p>`;
+
+// is an array? (true)
+arrOut.innerHTML += "<h3>isArray()</h3>";
+arrOut.innerHTML += `<p> - Is 'backPack' an Array?: <span class="posAnswer">` + Array.isArray(backPack); + `</span></p>`;
+
+// is instanceof array (true)
+arrOut.innerHTML += "<h3>instanceof</h3>";
+arrOut.innerHTML += `<p> - Is 'backPack' an 'instanceof' Array?: <span class="posAnswer">` + (backPack instanceof Array) + `</span></p>`; 
+arrOut.innerHTML += `<p> - Is 'backPack' an 'instanceof' String?: <span class="negAnswer">` + (backPack instanceof String) + `</span></p>`; 
+
+// .pop() / .push()
+arrOut.innerHTML += "<h3>.pop() / .push()</h3>";
+arrOut.innerHTML += `<p> - 'backPack.pop()' returns: <span class="posAnswer">` + (backPack.pop()) + `</span></p>`; 
+arrOut.innerHTML += `<p> - 'backPack.push('Grabthars Hammer')' returns new array length: <span class="posAnswer">` + (backPack.push("Grabthars Hammer")) + `</span></p>`; 
+arrOut.innerHTML += `<p> - arrayName.toString(): <span class="posAnswer">${backPack.toString()}</span></p>`;
+
+// .shift() / .unshift()
+arrOut.innerHTML += "<h3>.shift() / .unshift()</h3>";
+arrOut.innerHTML += `<p> - 'backPack.shift()' returns: <span class="posAnswer">` + (backPack.shift()) + `</span></p>`; 
+arrOut.innerHTML += `<p> - 'backPack.unshift('Carp')' returns new array length: <span class="posAnswer">` + (backPack.unshift("Carp")) + `</span></p>`; 
+arrOut.innerHTML += `<p> - arrayName.toString(): <span class="posAnswer">${backPack.toString()}</span></p>`;
+
+// .length
+arrOut.innerHTML += "<h3>.length</h3>";
+arrOut.innerHTML += `<p> - 'backPack.length' returns: <span class="posAnswer">` + (backPack.length) + `</span></p>`; 
+
+// .splice() / .slice()
+arrOut.innerHTML += "<h3>.splice() / .slice()</h3>";
+arrOut.innerHTML += `<p> - ADD TEXT AREA AND BUTTON HERE: <span class="negAnswer">text area + button go here.</span></p>`
+
+//
+// Arrays Area END
+//
+
+//
+// Loops Area
+//
+
+
+
+//
+// Loops Area END
+//
+
+
 
